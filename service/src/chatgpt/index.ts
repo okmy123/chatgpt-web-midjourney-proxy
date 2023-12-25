2312b83f9a9a5328c5c9edf9a192f75281258b6b
okmy123 <136219898+okmy123@users.noreply.github.com>
2023-12-26 03:16:52 +0800

    Update index.ts

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -15,7 +15,7 @@ const { HttpsProxyAgent } = httpsProxyAgent
 dotenv.config()
 
 const ErrorCodeMessage: Record<string, string> = {
-  401: '[OpenAI] 提供错误的API密钥 | Incorrect API key provided',
+  401: '[OpenAI] 提供错误的小巴士密钥 | Incorrect API key provided',
   403: '[OpenAI] 服务器拒绝访问，请稍后再试 | Server refused to access, please try again later',
   502: '[OpenAI] 错误的网关 |  Bad Gateway',
   503: '[OpenAI] 服务器繁忙，请稍后再试 | Server is busy, please try again later',
